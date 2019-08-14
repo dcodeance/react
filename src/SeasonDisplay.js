336fb4e561e5f689122d7cfaf9a22d2133eb343e
decodance <sushmithau2@gmail.com>
2019-08-14 15:39:01 +0530

    Season display

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './SeasonDisplay.css';
+const seasonConfig = {
+    summer: {
+        text: 'Lets hit the beach',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr, it is chilly',
+        iconName: 'snowflake'
+    }
+};
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+};
+const SeasdonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    return (<div className={`season-display ${season}`}>
+        <i className={`icon-left massive ${seasonConfig[season].iconName} icon`}/>
+        <h1>{seasonConfig[season].text}</h1>
+        <i className={`icon-right massive ${seasonConfig[season].iconName} icon`}/>
+    </div>);
+};
+
+export default SeasdonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,70 @@
-// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasdonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-
-
-function getButtonText() {
-    return 'Click on me!';
-}
-// Creat a react component
-const App =  () => {
-    //const buttonText = ['Click Me!', 2];
-    const buttonText = {text: 'Click Me!'};
-    const style = {backgroundColor: 'blue', color: 'white'};
-    return (
-        <div>
-            <label htmlFor='name' className='label'>Enter name:</label>
-            <input id='name' type='text'/>
-            <button style={{backgroundColor: 'blue', color: 'white'}}>
-                {buttonText.text}
-            </button>
-        </div>
+/* const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+            console.log(position)
+        },
+        (err) => {
+            console.log(err);
+        }
     );
-}
+    return (
+        <div>Hi there</div>
+    );
+};
+ */
 
-// Take the react component and show it on the screen
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log(props);
+        this.state = { lat: null, lan: null, errorMsg: ''};
+    }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                console.log(position);
+                this.setState({ lat: position.coords.latitude, lan: position.coords.longitude})
+            },
+            (err) => {
+                this.setState({errorMsg: err.message});
+                console.log(err);
+            }
+        );
+    }
+
+    
+    renderContent() {
+        if(this.state.errorMsg && !this.state.lat) {
+            return <div>Error: {this.state.errorMsg}</div>
+        }
+        if(!this.state.errorMsg && this.state.lat) {
+            return <SeasdonDisplay lat={this.state.lat}/>
+        }
+        return <Spinner message="Please accept location request"/>;
+    }
+    // React says we have to define render!!
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
+        /* return (
+            <div>
+                Latitude: {this.state.lat}
+                <br/>
+                Error: {this.state.errorMsg}
+            </div>
+        ); */
+    }
+}
 ReactDOM.render(
     <App/>,
-    document.querySelector('#root')
+    document.getElementById('root')
 );
