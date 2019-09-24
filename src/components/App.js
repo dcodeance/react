dd19cf05b42872dd9e180a5e44e2f119dc2e0bb3
Shettigar <Sushmitha.Shettigar@CenturyLink.com>
2019-09-24 18:31:33 +0530

    context and compare context with redux

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import { LanguageStore } from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LaunguageSelector';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="ui container">
+                <LanguageStore>
+                    <LanguageSelector />
+                    <ColorContext.Provider value="red">
+                            <UserCreate />
+                    </ColorContext.Provider>
+                </LanguageStore>
+                {/* <LanguageContext.Provider value='dutch'>
+                    <UserCreate />
+                </LanguageContext.Provider>
+                <UserCreate /> */}
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class Button extends React.Component {
+    renderButton(color) {
+        return (
+            <button className={`ui button ${color}`}>
+                <LanguageContext.Consumer>
+                    {({ language }) => language === 'english'? 'Submit' : 'Voorleggen'}
+                </LanguageContext.Consumer>
+            </button>
+        );
+    }
+
+    render() {
+        return (
+            <ColorContext.Consumer>
+                {(color) => 
+                    this.renderButton(color)
+                }
+            </ColorContext.Consumer>
+            
+        )
+    }
+}
+//Button.contextType = LanguageContext;
+
+export default Button;
